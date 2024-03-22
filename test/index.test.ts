e8bb9ca79b80b6b9a18dd50295c53b4826cf84de
NamesMT <dangquoctrung123@gmail.com>
2024-03-22 15:21:55 +0000

    tests(index): implement basic handler test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,34 @@
-import { expect, it } from 'vitest'
-import { logger } from '~/logger'
+import { describe, expect, it } from 'vitest'
+import { Hono } from 'hono'
+import { klona } from 'klona'
+import sampleEvent from './sample-event-v2.json'
+import { handle, streamHandle } from '~/handler'
 
-it('logger', () => {
-  expect(logger).toHaveProperty('info')
+function makeSampleEvent({ path = 'path', method = 'GET' }) {
+  const event = klona(sampleEvent)
+
+  event.rawPath = event.requestContext.http.path = path
+  event.routeKey = event.requestContext.routeKey = `${method} ${path}`
+  event.requestContext.http.method = method
+
+  return event
+}
+
+describe('basic', () => {
+  describe('normal handler', () => {
+    const app = new Hono()
+
+    // Registering routes
+    app.get('/', c => c.text('Hello Hono!'))
+
+    // Create handler
+    const handler = handle(app)
+
+    it('should process some basic events flawlessly 💅', async () => {
+      expect(handler(makeSampleEvent({ path: '/' }))).resolves.toMatchObject({ body: 'Hello Hono!' })
+    })
+  })
+
+  describe.skip('stream handler', () => {
+  })
 })
