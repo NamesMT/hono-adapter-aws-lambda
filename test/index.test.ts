d0e656925f1fb4992a5d53d496e6a317e2f02504
NamesMT <dangquoctrung123@gmail.com>
2024-03-28 09:02:35 +0000

    tests: update tests to current state

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { Hono } from 'hono'
 import { klona } from 'klona'
 import sampleEvent from './sample-event-v2.json'
-import { handle, streamHandle } from '~/handler'
+import { fixTriggerRoutes, getTriggerPath, handle, streamHandle } from '~/handler'
 
 function makeSampleEvent({ path = 'path', method = 'GET' }) {
   const event = klona(sampleEvent)
@@ -28,7 +28,7 @@ describe('basic', () => {
       // Registering routes
       app.get('/', c => c.text('Hello Hono!'))
       app.get('/hi', c => c.text('Hi Hono!'))
-      app.on('TRIGGER', 'test:simple', c => c.text('Hello Trigger Event!'))
+      app.on('TRIGGER', getTriggerPath('test:simple'), c => c.text('Hello Trigger Event!'))
 
       // Create handler
       const handler = handle(app)
@@ -40,15 +40,16 @@ describe('basic', () => {
       })
     })
 
-    describe('with basePath', () => {
+    describe('with basePath (fixTriggerRoutes())', () => {
       const app = new Hono().basePath('/based')
 
       // Registering routes
       app.get('/', c => c.text('Hello Hono!'))
       app.get('/hi', c => c.text('Hi Hono!'))
-      app.on('TRIGGER', 'test:simple', c => c.text('Hello Trigger Event!'))
+      app.on('TRIGGER', getTriggerPath('test:simple'), c => c.text('Hello Trigger Event!'))
 
       // Create handler
+      fixTriggerRoutes(app)
       const handler = handle(app)
 
       it('should process some basic events flawlessly 💅', async () => {
@@ -59,27 +60,25 @@ describe('basic', () => {
     })
 
     // TODO: support trigger events in route grouping
-    describe('with route grouping)', () => {
+    describe('with route grouping (fixTriggerRoutes())', () => {
       const grouped = new Hono()
 
       // Registering routes
       grouped.get('/', c => c.text('Hello Hono!'))
       grouped.get('/hi', c => c.text('Hi Hono!'))
-      grouped.on('TRIGGER', 'test:simple', c => c.text('Hello Trigger Event!'))
+      grouped.on('TRIGGER', getTriggerPath('test:simple'), c => c.text('Hello Trigger Event!'))
 
       const app = new Hono()
       app.route('/grouped', grouped)
 
       // Create handler
+      fixTriggerRoutes(app)
       const handler = handle(app)
 
       it('should process some basic events flawlessly 💅', async () => {
         expect(handler(makeSampleEvent({ path: '/grouped' }))).resolves.toMatchObject({ body: 'Hello Hono!' })
         expect(handler(makeSampleEvent({ path: '/grouped/hi' }))).resolves.toMatchObject({ body: 'Hi Hono!' })
-      })
-
-      it('should fail trigger event', async () => {
-        expect(handler(makeTriggerEvent('test:simple'))).resolves.toMatchObject({ body: '404 Not Found' })
+        expect(handler(makeTriggerEvent('test:simple'))).resolves.toMatchObject({ body: 'Hello Trigger Event!' })
       })
     })
   })
